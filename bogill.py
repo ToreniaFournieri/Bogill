594b4e93994a231b19eaeab5be108dec3dad5c91
Torenia.Founieri <torenia.fournieri@gmail.com>
2023-09-30 12:09:55 +0900

    save system

--- bogill.py
+++ bogill.py
@@ -1,9 +1,14 @@
+import streamlit as st
 import random
 
 # Constants
 NUM_ATTRIBUTES = 20
 MAX_ATTRIBUTE_VALUE = 5  # The maximum value an attribute can have
-MAX_MONSTER_STRENGTH = 5
+MAX_MONSTER_STRENGTH = 9
+
+# Define a path for the progress file
+PROGRESS_FILE = "battle_progress.txt"
+
 
 class Player:
     """Defines a player with health and attributes."""
@@ -12,7 +17,7 @@ class Player:
         self.health = health
 
     def display_status(self):
-        return f"Health: {self.health}, Attributes: {self.attributes}"
+        return f"HP: {self.health}, \n性能: {self.attributes}"
 
     def take_damage(self, damage):
         self.health -= damage
@@ -30,7 +35,7 @@ class Monster:
         self.attribute_value = attribute_value
 
     def display_status(self):
-        return f"Strength: {self.strength}, Attribute Order: {self.attribute_order}, Attribute Value: {self.attribute_value}"
+        return f"力: {self.strength}, \n性能: {self.attribute_order}, \n性能値: {self.attribute_value}"
 
 
 def create_monster():
@@ -55,13 +60,77 @@ def battle(player, monster):
     if total_damage > 0:
         player.take_damage(total_damage)
 
-    return 'Player wins!' if player.is_alive() else 'Player loses!'
+    return '勝利' if player.is_alive() else '敗北'
 
+def save_progress(player):
+    """Save the player's progress to a file."""
+    with open(PROGRESS_FILE, 'w') as file:
+        file.write(str(player.health) + '\n')
+        file.write(','.join(map(str, player.attributes)))
 
+def load_progress():
+    """Load the player's progress from a file."""
+    try:
+        with open(PROGRESS_FILE, 'r') as file:
+            health_line = file.readline().strip()
+            attributes_line = file.readline().strip()
 
-# Testing the game
-player = Player(20, (1, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
-monster = Monster(5,1,10)
-result = battle(player, monster)
+            # Check if the lines are not empty and can be processed
+            if not health_line or not attributes_line:
+                return None
 
-print(f"{player.display_status()} ,\n{monster.display_status()},\n{result}")
+            health = int(health_line)
+            attributes = tuple(map(int, attributes_line.split(',')))
+
+            # Check if the loaded attributes match the expected number of attributes
+            if len(attributes) != NUM_ATTRIBUTES:
+                return None
+
+            return Player(health, attributes)
+    except (FileNotFoundError, ValueError):
+        # Handle file not found or value conversion errors
+        return None
+
+def main():
+    st.title("戦闘ブラウザゲーム")
+    
+    # Check if 'player' is already in the session state:
+    if 'player' not in st.session_state:
+        loaded_player = load_progress()
+        if loaded_player:
+            st.session_state.player = loaded_player
+        else:
+            st.session_state.player = Player(80, (1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+
+    player = st.session_state.player
+
+    st.subheader("主人公 ステータス:")
+    st.text(player.display_status())
+
+    if player.is_alive():
+        if st.button("敵と戦う"):
+            # Create a monster
+            #monster = Monster(5,1,10)
+            monster = create_monster()
+
+            st.subheader("敵ステータス:")
+            st.text(monster.display_status())
+
+            # Battle
+            result = battle(player, monster)
+            st.subheader("戦闘結果:")
+            st.write(result)
+
+            st.text(player.display_status())
+            # Save progress after battle
+            save_progress(player)
+    else:
+        st.subheader("Player has been defeated!")
+        if st.button("Restart Game"):
+            # Reset the player's status
+            st.session_state.player = Player(80, (1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+            save_progress(st.session_state.player)
+
+if __name__ == "__main__":
+    main()
+
